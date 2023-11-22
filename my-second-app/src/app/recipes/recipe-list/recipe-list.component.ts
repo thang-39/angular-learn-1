affc74c3b70565ff104759bbf3814f4d5ac6a32e
thang.tran1 <thang.tran1@mesoneer.io>
2023-11-22 10:06:50 +0700

    finish create recipe detail

--- my-second-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-second-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/gMQmtA0djv1T93uDazHumXG0Ss4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/gMQmtA0djv1T93uDazHumXG0Ss4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.foodandwine.com/thmb/gMQmtA0djv1T93uDazHumXG0Ss4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Gnocchi-Parisienne-FT-RECIPE0523-a2ca65b48ffc4569adf8baafd009dfaa.jpg'),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
